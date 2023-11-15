83ecb57d46d2856503e5999d11f5fa1aec10c113
Sara Nieto <saranieto208@gmail.com>
2023-11-15 23:46:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'Sara',
+        age: 25,
+        skills: skills
+    };
+    return member.skills;
+}
